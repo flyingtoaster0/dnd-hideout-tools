2627aa564037270cce4b3b1e38d78ab3929fb7af
Tim Schonberger <flyingtoaster0@gmail.com>
2015-08-02 23:12:29 -0400

    Added tests for missing classes

--- spec/models/spell_spec.rb
+++ spec/models/spell_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Spell, type: :model do
+
+  it 'should not return Barbarian spells' do
+    spells = Spell.find_by_class_type 'barbarian'
+
+    expect(spells).to be_empty
+  end
+
   it 'should return Bard spells' do
     spells = Spell.find_by_class_type 'bard'
 
@@ -19,10 +26,34 @@ RSpec.describe Spell, type: :model do
     expect(spells).not_to be_empty
   end
 
+  it 'should not return Fighter spells' do
+    spells = Spell.find_by_class_type 'fighter'
+
+    expect(spells).to be_empty
+  end
+
+  it 'should not return Monk spells' do
+    spells = Spell.find_by_class_type 'monk'
+
+    expect(spells).to be_empty
+  end
+
+  it 'should return Paladin spells' do
+    spells = Spell.find_by_class_type 'paladin'
+
+    expect(spells).not_to be_empty
+  end
+
   it 'should return Ranger spells' do
     spells = Spell.find_by_class_type 'ranger'
 
     expect(spells).not_to be_empty
+  end
+
+  it 'should not return Rogue spells' do
+    spells = Spell.find_by_class_type 'rogue'
+
+    expect(spells).to be_empty
   end
 
   it 'should return Sorcerer spells' do
